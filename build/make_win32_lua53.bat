02d9b1c5711a9d760212d8b237bcd65e2d5bb5e5
Jonny <jonny.hong91@gmail.com>
2019-01-19 14:08:28 +0800

    win

--- build/make_win32_lua53.bat
+++ build/make_win32_lua53.bat
@@ -1,5 +1,5 @@
 mkdir build32 & pushd build32
-cmake -G "Visual Studio 14 2015" ..
+cmake -G "Visual Studio 15 2017" ..
 IF %ERRORLEVEL% NEQ 0 cmake -G "Visual Studio 15 2017" ..
 popd
 cmake --build build32 --config Release
